6d7962beee19ba3174d52929689f2d5f3fdb1f3d
Benoît Burgener <benoit.burgener@liip.ch>
2023-11-04 13:01:38 +0100

    fix: ajust list item loader to new layout

--- packages/frontend/src/components/loaders/RecipeListItemLoader.vue
+++ packages/frontend/src/components/loaders/RecipeListItemLoader.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="overflow-hidden bg-alt-50 rounded-xl border-b border-alt-200">
     <ContentLoader
-      view-box="0 0 343 380"
+      view-box="0 0 343 109"
       :speed="2"
       primary-color="#E7DFEC"
       secondary-color="#F5F2F8"
       class="sm:hidden"
     >
-      <rect x="0" y="0" rx="0" ry="0" width="343" height="257" />
-      <rect x="16" y="273" rx="3" ry="3" width="311" height="30" />
-      <rect x="16" y="307" rx="3" ry="3" width="120" height="14" />
-      <rect x="16" y="336" rx="3" ry="3" width="240" height="25" />
+      <rect x="0" y="0" rx="0" ry="0" width="114" height="109" />
+      <rect x="130" y="14" rx="3" ry="3" width="196" height="24" />
+      <rect x="130" y="44" rx="3" ry="3" width="70" height="18" />
+      <rect x="130" y="76" rx="3" ry="3" width="110" height="20" />
     </ContentLoader>
     <ContentLoader
       view-box="0 0 718 150"
