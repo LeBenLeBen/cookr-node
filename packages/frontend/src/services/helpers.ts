1de91f0b38f3c081508a21daf03501a31b2430a3
Benoît Burgener <benoit.burgener@liip.ch>
2024-04-01 15:32:21 +0200

    Provide auth token when uploading files

--- packages/frontend/src/services/helpers.ts
+++ packages/frontend/src/services/helpers.ts
@@ -1,12 +1,19 @@
 import { Directus_Files } from '@/gql/graphql';
 
+import store from '@/store';
+
 export function upload(file: File): Promise<Directus_Files> {
   const formData = new FormData();
   formData.append('file', file);
 
+  const headers = store.state.value.auth?.accessToken
+    ? { Authorization: `Bearer ${store.state.value.auth?.accessToken}` }
+    : undefined;
+
   return fetch('/files', {
     method: 'POST',
     body: formData,
+    headers,
   })
     .then((response) => response.json())
     .then((response) => response.data);
